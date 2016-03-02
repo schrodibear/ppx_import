2fd6deb8dbc390a42670f1aaf95427cc87dde6f1
Mikhail Mandrykin <mandrykin@ispras.ru>
2016-03-02 18:26:46 +0300

    Added support for equality constraints on objects and poly-variants expresed through type variables

--- src/ppx_import.ml
+++ src/ppx_import.ml
@@ -87,162 +87,215 @@ let rec longident_of_path path =
   | Path.Pdot (path, name, _) -> Ldot (longident_of_path path, name)
   | Path.Papply (lhs, rhs) -> Lapply (longident_of_path lhs, longident_of_path rhs)
 
-let rec core_type_of_type_expr ~subst ?(varsubst=[]) =
-  let core_type_of_type_expr t = core_type_of_type_expr ~subst ~varsubst t in
-  fun type_expr ->
+module H = Hashtbl.Make (struct type t = type_expr let equal t1 t2 = t1 == t2 let hash t = t.id end)
+
+let fresh_var, refresh_var_scope =
+  let counter = ref ~-1 in
+  (fun () ->
+    incr counter;
+    "tv__" ^ string_of_int !counter),
+  (fun () -> counter := -1)
+
+let rec core_type_of_type_expr =
+  let rec has_row te =
+    match te.desc with
+    | Tfield (_, _, _, ts) -> has_row ts
+    | Tnil                 -> false
+    | Tvar _               -> true
+    | _                    -> assert false
+  in
+  fun ~equality_cache ~subst ?(varsubst=[]) ->
+    let core_type_of_type_expr t = core_type_of_type_expr ~equality_cache ~subst ~varsubst t in
     let named ~mk path args =
       let lid  = longident_of_path path in
       let args = (List.map core_type_of_type_expr args) in
       begin match List.assoc lid subst with
-      | { ptyp_desc = Ptyp_constr (lid, _) } as typ ->
-        { typ with ptyp_desc = (mk lid args).ptyp_desc }
-      | _ -> assert false
-      | exception Not_found ->
-        mk { txt = lid; loc = !default_loc } args
+        | { ptyp_desc = Ptyp_constr (lid, _) } as typ ->
+          { typ with ptyp_desc = (mk lid args).ptyp_desc }
+        | _ -> assert false
+        | exception Not_found ->
+          mk { txt = lid; loc = !default_loc } args
       end
     in
-    match type_expr.desc with
-    | Tvar None | Tvar (Some "_") -> Typ.any ()
-    | Tvar (Some var) -> (try Typ.var (List.assoc var varsubst) with Not_found -> Typ.var var)
-    | Tarrow (label, lhs, rhs, _) ->
-      Typ.arrow label (core_type_of_type_expr lhs) (core_type_of_type_expr rhs)
-    | Ttuple xs ->
-      Typ.tuple (List.map core_type_of_type_expr xs)
-    | Tconstr (path, args, _) -> named ~mk:Typ.constr path args
-    | Tvariant { row_fields; row_closed } ->
-      let poly = ref None in
-      let fields =
-        row_fields |> List.map (fun (label, row_field) ->
-            let rec mapper =
-              function
-              | Rpresent None -> Rtag (label, [], true, [])
-              | Rpresent (Some ttyp) ->
-                Rtag (label, [], false, [core_type_of_type_expr ttyp])
-              | Reither (_, _, _, { contents = Some rf }) -> mapper rf
-              | Reither (flag, ttys, _, _) ->
-                poly := Some [];
-                Rtag (label, [], flag, List.map core_type_of_type_expr ttys)
-              | _ -> assert false
-            in
-            mapper row_field)
+    let subst_var v = try List.assoc v varsubst with Not_found -> v in
+    fun type_expr ->
+      let cache_result ~is_polymorphic r =
+        if not is_polymorphic then r
+        else
+          let v = fresh_var () in
+          H.add equality_cache type_expr v;
+          Typ.alias r v
       in
-      Typ.variant fields (if row_closed then Closed else Open) !poly
-    | Tobject (_, { contents = Some (path, args) }) -> named ~mk:Typ.class_ path @@ List.tl args
-    | Tobject (fds, { contents = None }) ->
-      let rec fields t =
-        match t.desc with
-        | Tfield (s, Fpresent, t, ts)     ->
-          let varopt, ts = fields ts in
-                                             varopt, (s, [], core_type_of_type_expr t) :: ts
-        | Tnil                            -> None, []
-        | Tvar None
-        | Tvar (Some "_")                 -> Some None, []
-        | Tvar (Some v)                   -> Some (Some (try List.assoc v varsubst with Not_found -> v)), []
-        | _                               -> assert false
-      in
-      begin match fields fds with
-      | None,          fields      -> Typ.object_ fields Closed
-      | Some None,     fields      -> Typ.object_ fields Open
-      | Some (Some v), fields      -> Typ.alias (Typ.object_ fields Open) v
-      end
-    | Tpoly (ty, tys) ->
-      let tys =
-        List.map
-          (fun ty ->
-             ty |>
-             core_type_of_type_expr |>
-             function
-             | { ptyp_desc = Ptyp_var s; _ } -> s
-             | _ ->
-               raise_errorf "Unsupported type description: constrained explicit polymorphism, e.g. 'a. ([> ] as 'a)")
-          tys
-      in
-      Typ.poly tys (core_type_of_type_expr ty)
-    | Tpackage _ -> raise_errorf "Unsupported type description: package, e.g. (module S)"
-    | Tnil | Tfield _ | Tlink _ | Tsubst _ | Tunivar _ -> assert false
+      match type_expr.desc with
+      | Tvar None | Tvar (Some "_") -> Typ.any ()
+      | Tvar (Some var) -> Typ.var (subst_var var)
+      | Tarrow (label, lhs, rhs, _) ->
+        Typ.arrow label (core_type_of_type_expr lhs) (core_type_of_type_expr rhs)
+      | Ttuple xs ->
+        Typ.tuple (List.map core_type_of_type_expr xs)
+      | Tconstr (path, args, _) -> named ~mk:Typ.constr path args
+      | Tvariant { row_fixed }
+        when not row_fixed && H.mem equality_cache type_expr ->
+        Typ.var (H.find equality_cache type_expr)
+      | Tvariant { row_fixed; row_fields; row_closed; row_more } ->
+        let poly = ref None in
+        let fields =
+          row_fields |> List.map (fun (label, row_field) ->
+              let rec mapper =
+                function
+                | Rpresent None -> Rtag (label, [], true, [])
+                | Rpresent (Some ttyp) ->
+                  Rtag (label, [], false, [core_type_of_type_expr ttyp])
+                | Reither (_, _, _, { contents = Some rf }) -> mapper rf
+                | Reither (flag, ttys, _, _) ->
+                  poly := Some [];
+                  Rtag (label, [], flag, List.map core_type_of_type_expr ttys)
+                | _ -> assert false
+              in
+              mapper row_field)
+        in
+        let ptyp = Typ.variant fields (if row_closed then Closed else Open) !poly in
+        let rec finish row_more =
+          match row_more.desc with
+          | Tvar (Some "_" | None)
+          | Tnil
+          | Tconstr (_, [], { contents = Mnil }) -> ptyp
+          | Tvar (Some v) when v <> "_"          -> Typ.alias ptyp (subst_var v)
+          | Tvariant { row_more }                -> finish row_more
+          | _                                    ->
+            raise_errorf "Unknown row variable description: %s" (Format.asprintf "%a" Printtyp.type_expr row_more)
+        in
+        cache_result ~is_polymorphic:(not row_fixed) @@ finish row_more
+      | Tobject (fds, _)
+        when has_row fds && H.mem equality_cache type_expr ->
+        Typ.var (H.find equality_cache type_expr)
+      | Tobject (fds, { contents = Some (path, args) }) ->
+        cache_result ~is_polymorphic:(has_row fds) @@ named ~mk:Typ.class_ path @@ List.tl args
+      | Tobject (fds, { contents = None }) ->
+        let rec fields t =
+          match t.desc with
+          | Tfield (s, Fpresent, t, ts)     ->
+            let varopt, ts = fields ts in
+            varopt, (s, [], core_type_of_type_expr t) :: ts
+          | Tnil                            -> None, []
+          | Tvar None
+          | Tvar (Some "_")                 -> Some None, []
+          | Tvar (Some v)                   -> Some (Some (subst_var v)), []
+          | _                               -> assert false
+        in
+        let r =
+          match fields fds with
+          | None,          fields      -> Typ.object_ fields Closed
+          | Some None,     fields      -> Typ.object_ fields Open
+          | Some (Some v), fields      -> Typ.alias (Typ.object_ fields Open) v
+        in
+        cache_result ~is_polymorphic:(has_row fds) r
+      | Tpoly (ty, tys) ->
+        let tys =
+          List.map
+            (fun ty ->
+               ty |>
+               core_type_of_type_expr |>
+               function
+               | { ptyp_desc = Ptyp_var s; _ } -> s
+               | _ ->
+                 raise_errorf "Unsupported type description: \
+                               constrained explicit polymorphism, e.g. 'a. ([> ] as 'a)")
+            tys
+        in
+        Typ.poly tys (core_type_of_type_expr ty)
+      | Tpackage _ -> raise_errorf "Unsupported type description: package, e.g. (module S)"
+      | Tnil | Tfield _ | Tlink _ | Tsubst _ | Tunivar _ -> assert false
 
-
-
-let ptype_decl_of_ttype_decl ?params ?manifest ~subst ptype_name ttype_decl =
-  let ptype_params, (varsubst, cstrs) =
-    match params with
-    | Some params when List.(length params = length ttype_decl.type_params) ->
-      params,
-      let varsubst =
+let ptype_decl_of_ttype_decl =
+  let equality_cache = H.create 32 in
+  let refresh_var_scope () =
+    H.clear equality_cache;
+    refresh_var_scope ()
+  in
+  fun ?params ?manifest ~subst ptype_name ttype_decl ->
+    refresh_var_scope ();
+    let ptype_params, (varsubst, cstrs) =
+      match params with
+      | Some params when List.(length params = length ttype_decl.type_params) ->
+        params,
+        let varsubst =
+          List.fold_left2
+            (fun acc orig (subst, _) ->
+               match orig.desc, subst.ptyp_desc with
+               | Tvar (Some v), Ptyp_var v' -> (v, v') :: acc
+               | _ -> acc)
+            []
+            ttype_decl.type_params params
+        in
+        varsubst,
         List.fold_left2
-          (fun acc orig (subst, _) ->
-             match orig.desc, subst.ptyp_desc with
-             | Tvar (Some v), Ptyp_var v' -> (v, v') :: acc
+          (fun acc orig (subst', _) ->
+             match orig.desc, subst'.ptyp_desc with
+             | Tvar _, _           -> acc
+             | _,      Ptyp_var v' ->
+               (v', core_type_of_type_expr ~equality_cache ~subst ~varsubst orig, subst'.ptyp_loc) :: acc
              | _ -> acc)
           []
           ttype_decl.type_params params
-      in
-      varsubst,
-      List.fold_left2
-          (fun acc orig (subst', _) ->
-            match orig.desc, subst'.ptyp_desc with
-            | Tvar _, _           -> acc
-            | _,      Ptyp_var v' -> (v', core_type_of_type_expr ~subst ~varsubst orig, subst'.ptyp_loc) :: acc
-            | _ -> acc)
-          []
-          ttype_decl.type_params params
-    | Some params ->
-      raise_errorf ~loc:ptype_name.loc
-        "A type with %d parameter(s) can't be imported as type `%s' with %d parameter(s)"
-        (List.length ttype_decl.type_params) ptype_name.txt (List.length params)
-    | None ->
-      List.map2 (fun param variance ->
-          core_type_of_type_expr ~subst param,
-          Invariant (* TODO *))
-        ttype_decl.type_params ttype_decl.type_variance,
-      ([], [])
-  in
-  let core_type_of_type_expr = core_type_of_type_expr ~subst ~varsubst in
-  let ptype_kind =
-    match ttype_decl.type_kind with
-    | Type_abstract -> Ptype_abstract
-    | Type_open -> Ptype_open
-    | Type_record (labels, _) ->
-      Ptype_record (labels |> List.map (fun ld ->
-        { pld_name       = { txt = ld.ld_id.name; loc = ld.ld_loc };
-          pld_mutable    = ld.ld_mutable;
-          pld_type       = core_type_of_type_expr ld.ld_type;
-          pld_loc        = ld.ld_loc;
-          pld_attributes = ld.ld_attributes; }))
-    | Type_variant constrs ->
-      Ptype_variant (constrs |> List.map (fun cd ->
-        { pcd_name       = { txt = cd.cd_id.name; loc = cd.cd_loc };
-          pcd_args       =
-            (match cd.cd_args with
-             | Cstr_tuple typs -> Pcstr_tuple (List.map core_type_of_type_expr typs)
-             | Cstr_record lds ->
-               Pcstr_record
-                 (List.map
-                    (fun { ld_id = { name = txt };
-                           ld_mutable = pld_mutable;
-                           ld_type;
-                           ld_loc = pld_loc;
-                           ld_attributes = pld_attributes } ->
-                      { pld_name = { txt; loc = pld_loc };
-                        pld_mutable;
-                        pld_type = core_type_of_type_expr ld_type;
-                        pld_loc;
-                        pld_attributes })
-                    lds));
-          pcd_res        = (match cd.cd_res with Some x -> Some (core_type_of_type_expr x)
-                                               | None -> None);
-          pcd_loc        = cd.cd_loc;
-          pcd_attributes = cd.cd_attributes; }))
-  and ptype_manifest =
-    match ttype_decl.type_manifest with
-    | Some typ -> Some (core_type_of_type_expr typ)
-    | None -> manifest
-  in
-  { ptype_name; ptype_params; ptype_kind; ptype_manifest;
-    ptype_cstrs      = List.map (fun (v, ct, loc) -> Typ.var v, ct, loc) cstrs;
-    ptype_private    = Public;
-    ptype_attributes = ttype_decl.type_attributes;
-    ptype_loc        = ttype_decl.type_loc; }
+      | Some params ->
+        raise_errorf ~loc:ptype_name.loc
+          "A type with %d parameter(s) can't be imported as type `%s' with %d parameter(s)"
+          (List.length ttype_decl.type_params) ptype_name.txt (List.length params)
+      | None ->
+        List.map2 (fun param variance ->
+            core_type_of_type_expr ~equality_cache ~subst param,
+            Invariant (* TODO *))
+          ttype_decl.type_params ttype_decl.type_variance,
+        ([], [])
+    in
+    let core_type_of_type_expr = core_type_of_type_expr ~equality_cache ~subst ~varsubst in
+    let ptype_kind =
+      match ttype_decl.type_kind with
+      | Type_abstract -> Ptype_abstract
+      | Type_open -> Ptype_open
+      | Type_record (labels, _) ->
+        Ptype_record (labels |> List.map (fun ld ->
+            { pld_name       = { txt = ld.ld_id.name; loc = ld.ld_loc };
+              pld_mutable    = ld.ld_mutable;
+              pld_type       = core_type_of_type_expr ld.ld_type;
+              pld_loc        = ld.ld_loc;
+              pld_attributes = ld.ld_attributes; }))
+      | Type_variant constrs ->
+        Ptype_variant (constrs |> List.map (fun cd ->
+            refresh_var_scope ();
+            { pcd_name       = { txt = cd.cd_id.name; loc = cd.cd_loc };
+              pcd_args       =
+                (match cd.cd_args with
+                 | Cstr_tuple typs -> Pcstr_tuple (List.map core_type_of_type_expr typs)
+                 | Cstr_record lds ->
+                   Pcstr_record
+                     (List.map
+                        (fun { ld_id = { name = txt };
+                               ld_mutable = pld_mutable;
+                               ld_type;
+                               ld_loc = pld_loc;
+                               ld_attributes = pld_attributes } ->
+                          { pld_name = { txt; loc = pld_loc };
+                            pld_mutable;
+                            pld_type = core_type_of_type_expr ld_type;
+                            pld_loc;
+                            pld_attributes })
+                        lds));
+              pcd_res        = (match cd.cd_res with Some x -> Some (core_type_of_type_expr x)
+                                                   | None -> None);
+              pcd_loc        = cd.cd_loc;
+              pcd_attributes = cd.cd_attributes; }))
+    and ptype_manifest =
+      match ttype_decl.type_manifest with
+      | Some typ -> Some (core_type_of_type_expr typ)
+      | None -> manifest
+    in
+    { ptype_name; ptype_params; ptype_kind; ptype_manifest;
+      ptype_cstrs      = List.map (fun (v, ct, loc) -> Typ.var v, ct, loc) cstrs;
+      ptype_private    = Public;
+      ptype_attributes = ttype_decl.type_attributes;
+      ptype_loc        = ttype_decl.type_loc; }
 
 let subst_of_manifest { ptyp_attributes; ptyp_loc } =
   let rec subst_of_expr expr =
@@ -318,7 +371,7 @@ let rec psig_of_tsig ~subst ?(trec=[]) tsig =
     { psig_desc = Psig_value {
         pval_name = Location.mknoloc name; pval_loc = val_loc;
         pval_attributes = val_attributes;
-        pval_prim; pval_type = core_type_of_type_expr ~subst val_type; };
+        pval_prim; pval_type = core_type_of_type_expr ~equality_cache:(H.create 32) ~subst val_type; };
       psig_loc = val_loc } ::
     psig_of_tsig ~subst rest
   | [] -> []
